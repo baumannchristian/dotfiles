402f35443599a33235962427bb4284780eeb98d5
Christian Baumann <16853063+baumannchristian@users.noreply.github.com>
2025-07-08 21:19:02 +0200

    [nvim] show errors inline properly

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,3 +9,11 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+
+vim.diagnostic.config({
+  virtual_text = true,  -- show inline errors
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
